af4def35231711c082c170a9bb1723d79dbc7aad
Jose Chavarria <joserafaelcascante@gmail.com>
2017-07-05 16:29:06 -0600

    init import

---
+++ frontend/views/site/index.php
@@ -0,0 +1,53 @@
+<?php
+Yii::$app->site->getMetaTags();
+?>
+
+<div class="site-index">
+
+	<div class="jumbotron">
+		<h1>Just Blog</h1>
+		<p class="lead">
+			<span class="label label-info">Easy</span>
+			<span class="label label-success">Pretty</span>
+			<span class="label label-primary">Light</span>
+			<span class="label label-danger">Modern</span>
+		</p>
+	</div>
+
+	<div class="body-content">
+
+		<div class="row blog">
+			<div class="col-lg-4">
+				<h2>Heading</h2>
+
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
+					dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+					ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+					fugiat nulla pariatur.</p>
+
+					<p><a class="btn btn-default" href="http://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
+				</div>
+				<div class="col-lg-4">
+					<h2>Heading</h2>
+
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
+						dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+						ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+						fugiat nulla pariatur.</p>
+
+						<p><a class="btn btn-default" href="http://www.yiiframework.com/forum/">Yii Forum &raquo;</a></p>
+					</div>
+					<div class="col-lg-4">
+						<h2>Heading</h2>
+
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
+							dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+							ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+							fugiat nulla pariatur.</p>
+
+							<p><a class="btn btn-default" href="http://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a></p>
+						</div>
+					</div> 
+
+				</div>
+			</div>
